d6625295a34d83ee32b2ea823e54f823b5e6fc28
James Drever <james@careful.digital>
2024-11-20 15:15:37 +0000

    Updating naming

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Kirby\Cms\App as Kirby;
 @include_once __DIR__ . '/vendor/autoload.php';
 
 Kirby::plugin(
-    'bsbi/bsbi-web-base', [
+    'open/web-base', [
             'blueprints' => require __DIR__ . '/blueprints.php',
         ]
 );
