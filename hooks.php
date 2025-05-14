28624562bd31b88fc6cebd2affb94d2f39f5cc55
James Drever <james@careful.digital>
2025-05-14 15:31:10 +0100

    Moving publish/create hooks > kirby-base

---
+++ hooks.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'page.update:after' => function ($newPage, $oldPage) {
+        $user = kirby()->user();
+
+        $newPage = $newPage->update([
+            'updatedDate' => date('Y-m-d H:i:s'),
+            'updatedBy' => $user?->id()
+        ]);
+        if ($newPage->publishedDate()->isEmpty()) {
+            $newPage = $newPage->update([
+                'publishedDate' => date('Y-m-d H:i:s'),
+                'publishedBy' => $user ? $user->id() : null
+            ]);
+        };
+        return $newPage;
+    },
+    'page.create:after' => function ($page) {
+        $user = kirby()->user();
+        $page = $page->update([
+            'publishedDate' => date('Y-m-d H:i:s'),
+            'publishedBy' => $user?->id()
+        ]);
+        return $page;
+    },
+];
