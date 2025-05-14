b53f7572caf174392c88c3006b3189ab13997367
James Drever <james@careful.digital>
2025-05-14 17:00:03 +0100

    home page hero block>$currentPage, and outline nav buttons

--- snippets/menu.php
+++ snippets/menu.php
@@ -35,7 +35,7 @@ foreach ($menuPages->getListItems() as $item) :
 endforeach; ?>
     </ul>
     <div class="ms-auto d-flex align-items-center" id="searchBar">
-        <form method="get" action="../../../../index.php" role="search">
+        <form method="get" action="/search" role="search">
             <input type="search" aria-label="Search" name="q" class="form-control-sm ms-2 me-2">
             <button class="btn btn-sm btn-success me-1" type="submit">Search</button>
         </form>
