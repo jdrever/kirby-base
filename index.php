3eb39ae01c15eace55af1ca675c12a1f222fcfd0
James Drever <james@careful.digital>
2025-05-15 12:03:50 +0100

    bsbi-web no longer a plugin

--- index.php
+++ index.php
@@ -12,5 +12,47 @@ Kirby::plugin(
             'templates' => [
                 'file_link' => __DIR__ . '/templates/file_link.php',
             ]
+
         ]
 );
+
+if (option('debug') === false) {
+// Set a global exception handler
+    set_exception_handler(function (Throwable $exception) {
+        $exceptionAsString = "Message: " . $exception->getMessage() . "\n" .
+            "File:" . $exception->getFile() . "'\n" .
+            "Line:" . $exception->getLine() . "\n" .
+            "Trace:" . $exception->getTraceAsString();
+
+        error_log($exceptionAsString);
+
+        if (!str_starts_with($_SERVER['HTTP_HOST'], 'localhost')) {
+            try {
+                $email = [
+                    'to' => option('adminEmail'),
+                    'from' => option('defaultEmail'),
+                    'subject' => 'Website Exception: ' . $exception->getMessage(),
+                    'body' => [
+                        'html' => "<b>An unhandled exception occurred:</b><br>" .
+                            "<b>Message</b>: " . $exception->getMessage() . "<br>" .
+                            "<b>File:</b> " . $exception->getFile() . "<br>" .
+                            "<b>Line:</b> " . $exception->getLine() . "<br>" .
+                            "<b>Trace:</b> " . $exception->getTraceAsString(),
+                    ]
+                ];
+                kirby()->email($email);
+            } catch (Throwable $exception) {
+                //continue if an exception occurs when sending the email
+            }
+        }
+
+        // Render the error page and pass the exception
+        $kirby = Kirby::instance();
+        $kirby->response()->code(500); // Set HTTP status code to 500
+        echo Tpl::load(__DIR__ . '/templates/error-500.php', [
+            'userRole' => $kirby->user() ? $kirby->user()->role()->name() : '',
+            'exception' => $exceptionAsString,
+        ]);
+        exit;
+    });
+}
