9836328796e7076337acb32b4cc7bd3cc5b5bb1d
James Drever <james@careful.digital>
2025-07-04 15:01:42 +0100

    basket/conferences/donate > content

--- snippets/blocks/file.php
+++ snippets/blocks/file.php
@@ -5,22 +5,17 @@ declare(strict_types=1);
 /**
  * @var App $kirby
  * @var Site $site
- * @var CustomDefaultPage $page
  * @var Block $block
  */
 
-use BSBI\Course\CustomDefaultPage;
 use Kirby\Cms\App;
 use Kirby\Cms\Block;
 use Kirby\Cms\Site;
 
 ?>
-<div class="container m-2 p-4 bg-light">
+<div class="list-group m-2 p-4">
 <?php if ($file = $block->file()->toFile()) : ?>  
-    <a href="<?= $file->url()?>" target="_blank"><?=$block->label() != "" ? $block->label() : $file->filename()?></a>
-    <a class="btn btn-primary btn-sm" href="<?= $file->url()?>" target="_blank">
-        VIEW
-    </a>
+    <a class="list-group-item" href="<?= $file->url()?>" target="_blank"><?=$block->label() != "" ? $block->label() : $file->filename()?> : VIEW</a>
 <?php else : ?>
     <p>No file</p>
 <?php endif?>
