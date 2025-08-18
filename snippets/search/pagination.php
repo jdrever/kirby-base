8fb246d768a21082c0de3f818ce1e61ba2d8620d
James Drever <james@careful.digital>
2025-08-18 16:36:04 +0100

    Only display first/last 3 pagination links

--- snippets/search/pagination.php
+++ snippets/search/pagination.php
@@ -9,6 +9,12 @@ if (!isset($pagination)) :
     return;
 endif;
 
+$currentPage = $pagination->getCurrentPage();
+$pageCount = $pagination->getPageCount();
+$limit = 3;
+$showEllipsisStart = false;
+$showEllipsisEnd = false;
+
 /** @var Pagination $pagination **/
 ?>
 
@@ -24,12 +30,34 @@ endif;
             <li class="page-item disabled">
                 <span class="page-link">&laquo;</span>
             </li>
-        <?php endif ?>
-        <?php for ($i = 1; $i <= $pagination->getPageCount(); $i++) : ?>
-            <li class="page-item<?= ($pagination->getCurrentPage() === $i) ? ' active' : '' ?>">
-                <a class="page-link" href="<?= $pagination->getPageURL($i) ?>"><?= $i ?></a>
-            </li>
-        <?php endfor ?>
+        <?php endif;
+        $lastPrintedPage = 0;
+        for ($i = 1; $i <= $pagination->getPageCount(); $i++) :
+            // Determine if the current page number should be displayed
+            $showPage = false;
+            // Always show the first `limit` pages
+            if ($i <= $limit) :
+                $showPage = true;
+            // Always show the last `limit` pages
+            elseif ($i > $pageCount - $limit) :
+                $showPage = true;
+            // Always show the current page and its direct neighbors
+            elseif (abs($i - $currentPage) <= 1) :
+                $showPage = true;
+            endif;
+
+            // Print the page number if it's within the rules
+            if ($showPage) :
+                // If there's a gap since the last printed page, show an ellipsis
+                if ($i > $lastPrintedPage + 1) :?>
+                    <li class="page-item disabled"><span class="page-link">...</span></li>
+                <?php endif ?>
+                <li class="page-item<?= ($currentPage === $i) ? ' active' : '' ?>">
+                    <a class="page-link" href="<?= $pagination->getPageURL($i) ?>"><?= $i ?></a>
+                </li>
+                <?php $lastPrintedPage = $i;
+            endif;
+        endfor ?>
         <?php if ($pagination->hasNextPage()) : ?>
             <li class="page-item">
                 <a class="page-link" href="<?= $pagination->getNextPageUrl() ?>" aria-label="Next">
