8bf48bf15251d1e6327d5bb918527e0f004742ba
James Drever <james@careful.digital>
2025-08-27 09:35:23 +0100

    Fixing pagination snippet

--- snippets.php
+++ snippets.php
@@ -19,7 +19,7 @@ return [
     'base/robots' => __DIR__ . '/snippets/robots.php',
     'base/full-width-block-starts' => __DIR__ . '/snippets/full-width-block-starts.php',
     'base/full-width-block-ends' => __DIR__ . '/snippets/full-width-block-ends.php',
-    'base/pagination' => __DIR__ . '/snippets/search/pagination.php',
+    'base/pagination' => __DIR__ . '/snippets/pagination.php',
 
     //colour mode
     'colour-mode/selector' => __DIR__ . '/snippets/colour-mode/selector.php',
