719b1a559f36e6ce5ac8d1d45c94a587d4824c36
James Drever <james@careful.digital>
2025-09-08 14:33:39 +0100

    More image optimisation changes

---
+++ src/models/ImageSizes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BSBI\WebBase\models;
+
+enum ImageSizes: string
+{
+    case NOT_SPECIFIED = '';
+    case HALF_LARGE_SCREEN = '(min-width: 768px) 50vw, 100vw';
+
+
+    // Add more types as needed
+}
