ef663160cdba0967e3e5a2eb771bb35b39910e03
James Drever <james@careful.digital>
2025-10-02 13:25:56 +0100

    adding file icon in block snippet

--- snippets/blocks/file.php
+++ snippets/blocks/file.php
@@ -14,8 +14,10 @@ use Kirby\Cms\Site;
 
 ?>
 <div class="list-group m-1 p-2">
-<?php if ($file = $block->file()->toFile()) : ?>  
-    <a class="list-group-item" href="<?= $file->url()?>" target="_blank"><?=$block->label() != "" ? $block->label() : $file->filename()?>: VIEW</a>
+<?php if ($file = $block->file()->toFile()) : ?>
+    <a class="list-group-item" href="<?= $file->url()?>" target="_blank">
+        <img src="/assets/images/icons/file-text.svg" alt="File icon">
+        <?=$block->label() != "" ? $block->label() : $file->filename()?>: VIEW</a>
 <?php else : ?>
     <p>No file</p>
 <?php endif?>
