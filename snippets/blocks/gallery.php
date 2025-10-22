7d419598e021b4d7cae29a4ed086f52e283cc12e
James Drever <james@careful.digital>
2025-10-22 10:26:29 +0100

    See All Images button on image gallery

--- snippets/blocks/gallery.php
+++ snippets/blocks/gallery.php
@@ -15,59 +15,100 @@ use Kirby\Cms\App;
 use Kirby\Cms\Block;
 use Kirby\Cms\Site;
 use Kirby\Toolkit\Str;
+use Kirby\Toolkit\Collection;
 
 $overallCaption = $block->caption();
 $crop    = $block->crop()->isTrue();
-$ratio   = $block->ratio()->or('auto');
+$ratio   = $block->ratio()->or('auto')->value();
 $fullWidth = $block->fullWidth()->toBool();
 $figCaptionClass = '';
 
-snippet('base/full-width-block-starts', ['fullWidth' => $fullWidth]) ?>
-<div class="container">
-    <div class="row row-cols-2">
-<?php foreach ($block->images()->toFiles() as $image) : ?>
-        <div class="col">
-<?php
+$allImages = $block->images()->toFiles();
+$showSeeAllButton = $block->showSeeAllButton()->toBool(); // NEW: Check for the new field
+
+/** @var Collection<Kirby\Cms\File> $visibleImages */
+$visibleImages = $allImages;
+/** @var Collection<Kirby\Cms\File>|null $hiddenImages */
+$hiddenImages = null;
+
+// Logic to split images if 'showSeeAllButton' is true and there are more than 2 images
+if ($showSeeAllButton && $allImages->count() > 2) {
+    $visibleImages = $allImages->slice(0, 2);
+    $hiddenImages  = $allImages->slice(2);
+}
+
+// Function to render a single image figure (to avoid repetition)
+$renderImage = function (Kirby\Cms\File $image, string $ratio, bool $crop, Block $block, string $imgClass, string $figCaptionClass) {
     $alt     = $image->alt();
     $caption = $image->caption();
-    //$crop    = $image->crop()->isTrue();
     $link    = $image->link();
-    //$ratio   = $image->ratio()->or('auto');
     $src     = null;
 
-    if ($block->fixedWidth()->isNotEmpty()) :
-        $image=$image->resize((int)$block->fixedWidth()->value());
-    endif;
+    if ($block->fixedWidth()->isNotEmpty()) {
+        $image = $image->resize((int)$block->fixedWidth()->value());
+    }
     $src = $image->url();
 
-    $imgClass="figure-img img-fluid";
-    $figCaptionClass="figure-caption";
-?>
-<?php if ($src): ?>
-<figure<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
-  <?php if ($link->isNotEmpty()): ?>
-  <a href="<?= Str::esc($link->toUrl()) ?>">
-    <img class="<?=$imgClass?>" src="<?= $src ?>" alt="<?= $alt->esc() ?>">
-  </a>
-  <?php else: ?>
-  <img class="<?=$imgClass?>" src="<?= $src ?>" alt="<?= $alt->esc() ?>">
-  <?php endif ?>
+    if ($src) : ?>
+        <figure<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
+            <?php if ($link->isNotEmpty()): ?>
+                <a href="<?= Str::esc($link->toUrl()) ?>">
+                    <img class="<?= $imgClass ?>" src="<?= $src ?>" alt="<?= $alt->esc() ?>">
+                </a>
+            <?php else: ?>
+                <img class="<?= $imgClass ?>" src="<?= $src ?>" alt="<?= $alt->esc() ?>">
+            <?php endif ?>
 
-  <?php if ($caption->isNotEmpty()): ?>
-  <figcaption class="<?=$figCaptionClass?>">
-    <?= $caption->kt() ?>
-  </figcaption>
-  <?php endif ?>
-</figure>
-<?php endif ?>
-        </div>
-<?php endforeach ?>
+            <?php if ($caption->isNotEmpty()): ?>
+                <figcaption class="<?= $figCaptionClass ?>">
+                    <?= $caption->kt() ?>
+                </figcaption>
+            <?php endif ?>
+        </figure>
+    <?php endif;
+};
+
+$imgClass="figure-img img-fluid";
+$figCaptionClass="figure-caption";
+?>
+
+<?php snippet('base/full-width-block-starts', ['fullWidth' => $fullWidth]) ?>
+<div class="container">
+    <div class="row row-cols-2">
+        <?php
+        // --- RENDER VISIBLE IMAGES (Max 2, or all if no split) ---
+        foreach ($visibleImages as $image) :
+            ?>
+            <div class="col">
+                <?php $renderImage($image, $ratio, $crop, $block, $imgClass, $figCaptionClass); ?>
+            </div>
+        <?php endforeach ?>
     </div>
+
+    <?php
+    // --- RENDER HIDDEN IMAGES BEHIND DETAILS/SUMMARY ---
+    if ($hiddenImages && $hiddenImages->count() > 0) :
+        ?>
+        <details class="as-accordion mt-2 p-0">
+            <summary class="mb-2">
+                See All Images
+            </summary>
+            <div class="row row-cols-2 pt-6">
+                <?php foreach ($hiddenImages as $image) : ?>
+                    <div class="col">
+                        <?php $renderImage($image, $ratio, $crop, $block, $imgClass, $figCaptionClass); ?>
+                    </div>
+                <?php endforeach ?>
+            </div>
+        </details>
+    <?php endif; ?>
+
 </div>
 <?php if ($overallCaption->isNotEmpty()): ?>
-<p class="<?=$figCaptionClass?>">
-    <?= $overallCaption ?>
-</p>
+    <p class="<?=$figCaptionClass?> mt-3">
+        <?= $overallCaption ?>
+    </p>
 <?php endif;
 
 snippet('base/full-width-block-ends', ['fullWidth' => $fullWidth]);
+?>
