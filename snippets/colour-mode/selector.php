4872f11a41760d3107f1c989ba2de0fe376b1e60
James Drever <james@careful.digital>
2025-11-04 16:05:11 +0000

    Adding suggested links to 404 page

--- snippets/colour-mode/selector.php
+++ snippets/colour-mode/selector.php
@@ -17,25 +17,38 @@ $colourMode = $currentPage->getColourMode();
 ?>
 <?php if ($colourMode !== 'light') : ?>
     <a href="<?=$currentPage->getUrl()?>?colourMode=light">
+<?php else : ?>
+    <strong>
 <?php endif ?>
     Light
 <?php if ($colourMode !== 'light') : ?>
     </a>
+<?php else : ?>
+    </strong>
 <?php endif ?>
+
     |
 <?php if ($colourMode !== 'dark') : ?>
     <a href="<?=$currentPage->getUrl()?>?colourMode=dark">
+<?php else : ?>
+    <strong>
 <?php endif ?>
     Dark
 <?php if ($colourMode !== 'dark') : ?>
     </a>
+<?php else : ?>
+    </strong>
 <?php endif ?>
     |
 <?php if ($colourMode !== 'auto') : ?>
     <a href="<?=$currentPage->getUrl()?>?colourMode=auto">
+<?php else : ?>
+    <strong>
 <?php endif ?>
     Auto
 <?php if ($colourMode !== 'auto') : ?>
     </a>
+<?php else : ?>
+    </strong>
 <?php endif;
 
