79c54748bfcf980f5e7b44202eeed74ce8fe15e1
James Drever <james@careful.digital>
2025-11-12 08:19:51 +0000

    Sorting 500 page

--- templates/error-500.php
+++ templates/error-500.php
@@ -1,16 +1,23 @@
 <?php
 /** @var string $userRole */
 /** @var string $exception */
-?>
-
-<?php snippet('base/header');
- ?>
-<?php snippet('logo') ?>
+snippet('base/styles');
+snippet('logo') ?>
+<!DOCTYPE html>
+<html lang="en"
+<head>
+    <meta charset="utf-8">
+    <title>Error</title>
+</head>
+<body>
 <div class="alert alert-danger" role="alert">
     <h2>An unexpected error has occurred </h2>
     <?php if ($userRole === 'admin') : ?>
         <p><?=$exception?></p>
+    <?php else : ?>
+    <p>This error will be reported to the website administrators.  We apologies for any incovenience.</p>
     <?php endif ?>
     <a href="/" class="btn btn-outline-primary">Return to the website.</a>
 </div>
-<?php echo('</body></html>');
+</body>
+</html>
