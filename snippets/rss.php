4cf279b4d43da3f50ba5b0e497127d4dc302be17
James Drever <james@careful.digital>
2026-01-15 06:59:23 +0000

    Add RSS feed endpoints and improve collection caching
    Introduced new RSS feed routes for blog posts, news, press releases, and events, enhancing content delivery. Improved collection handling by implementing caching to optimize performance and reduce redundant data queries. Fixed issues in the RSS snippet for better error handling and content formatting.

--- snippets/rss.php
+++ snippets/rss.php
@@ -1,10 +1,32 @@
-<?php echo '<?xml version="1.0" encoding="utf-8"?>'?>
-<?php echo '<?xml-stylesheet href="/assets/css/pretty-feed-v3.xsl" type="text/xsl"?>'?>
+<?php
+
+if (!isset($feedTitle)) : 
+    throw new Exception('No $feedTitle supplied');
+endif;
+
+if (!isset($feedLink)) : 
+    throw new Exception('No $feedLink supplied');
+endif;
+
+if (!isset($feedDescription)) : 
+    throw new Exception('No $feedDescription supplied');
+endif;
+
+if (!isset($feedUrl)) : 
+    throw new Exception('No $feedUrl supplied');
+endif;
+
+if (!isset($posts)) : 
+    throw new Exception('No $posts supplied');
+endif;
+
+echo '<?xml version="1.0" encoding="utf-8"?>';
+echo '<?xml-stylesheet href="/assets/css/pretty-feed-v3.xsl" type="text/xsl"?>' ?>
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/"  xmlns:atom="http://www.w3.org/2005/Atom" xmlns:georss="http://www.georss.org/georss" xmlns:gml="http://www.opengis.net/gml">
   <channel>
     <title><?=$feedTitle?></title>
     <link><?=$feedLink?></link>
-    <description>?=$feedDescription?></description>
+    <description><?=$feedDescription?></description>
     <language>en</language>
     <pubDate><?=date('r', time())?></pubDate>
     <lastBuildDate><?=date('r', time())?></lastBuildDate>
@@ -14,7 +36,7 @@
       <title><?=$post->title()?></title>
       <link><?=$post->url()?></link>
       <description>
-        <?=html($post->openingContent())?>
+        <?=$post->mainContent()->toBlocks()->excerpt(100)?>
       </description>
       <pubDate><?=date('r', $post->publishedDate()->toDate())?></pubDate>
     </item>
